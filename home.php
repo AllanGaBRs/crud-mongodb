63f45ad12f189d0bbfd892bd2da2a18f5b43cfd9
ArthurBLedur <143144102+ArthurBLedur@users.noreply.github.com>
2024-11-18 19:34:29 -0300

    tela inicial

---
+++ home.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container text-center mt-5">
+        <h1 class="mb-4">Bem-vindo</h1>
+        <p class="lead">Selecione uma opção abaixo para começar</p>
+    
+        <div class="">
+            <div class="">
+                <a href="tela_cadastro_curso.php" class="btn btn-primary">Cadastrar Cursos</a>
+            </div>
+            <div class="">
+                <a href="tela_cadastro_estudante.php" class="btn btn-success">Cadastrar Alunos</a>
+            </div>
+            <div class="">
+                <a href="tela_matricula.php" class="btn btn-primary">Matricular Alunos</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
